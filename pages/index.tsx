2399d106ddf57d76b398cb1b33c1c535c5e82688
Maharsh Patel <patel@maharsh.net>
2023-03-30 20:59:51 -0600

    fix: styles for main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,24 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import styles from '@/styles/Home.module.css';
+// import styles from '@/styles/Home.module.css';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 100px;
+`;
+
+const Project = styled.div`
+  font-family: var(--font-mono);
+  padding: 20px;
+  transition: 0.1s;
+  color: rgb(var(--foreground-rgb));
+  background-color: rgb(var(--background-rgb));
+
+  &:hover {
+    color: rgb(var(--background-rgb));
+    background-color: rgb(var(--foreground-rgb));
+  }
+`;
 
 export default function Home() {
   return (
@@ -11,13 +29,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            <Link href="/proximity">Proximity</Link>
-          </p>
-        </div>
-      </main>
+      <Container>
+        <Project>
+          <Link href="/proximity">Proximity</Link>
+        </Project>
+        {/* <Project>
+          <Link href="/proximity">Proximity</Link>
+        </Project> */}
+      </Container>
     </>
   );
 }
