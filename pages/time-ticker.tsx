fd7f4b8fcb4f55d08d0eec57d683b10d069ea50d
Maharsh Patel <patel@maharsh.net>
2023-05-29 21:33:19 -0600

    feat: add hour ticker labels

--- pages/time-ticker.tsx
+++ pages/time-ticker.tsx
@@ -7,6 +7,7 @@ type TickerProps = {
 
 const Card = styled.div`
   max-width: 600px;
+  min-width: 300px;
   height: 20vh;
   // background-color: rgb(var(--foreground-rgb));
   border: 1px solid rgb(var(--foreground-rgb));
@@ -24,26 +25,46 @@ const Card = styled.div`
 const TickerContainer = styled.div`
   display: flex;
   height: 8px;
+  margin-left: 5px;
+  margin-right: 5px;
+  justify-content: space-between;
 `;
 
 const Ticker = styled.div<TickerProps>`
   // border-left: 1px solid rgb(var(--foreground-rgb));
   border-left: ${(props) => (props.hourMark ? '1px solid #000000' : '1px solid #afafaf')};
-  width: calc(1.0309278%); // 100 / 97 = 1.0309278 -> number of ticks in the day
+  // width: calc(1.0309278%); // 100 / 97 = 1.0309278 -> number of ticks in the day
 `;
+
+const TickerLabelContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 12px;
+  line-height: 16px;
+  font-weight: 500px;
+`;
+const TickerLabel = styled.div``;
 
 export function Tickers() {
   const ticks = [];
   const ticksPerDay = 24 * 4 + 1; // +1 for midnight of next day
   for (let i = 0; i < ticksPerDay; i++) {
-    if (i % 4 === 0) {
-      ticks.push(<Ticker hourMark />);
-    } else {
-      ticks.push(<Ticker />);
-    }
+    ticks.push(<Ticker key={i} hourMark={i % 4 === 0} />);
   }
 
   return <TickerContainer>{ticks}</TickerContainer>;
+}
+
+export function TickerLabels() {
+  const labels = [12, 6, 12, 6, 12];
+
+  return (
+    <TickerLabelContainer>
+      {labels.map((label) => (
+        <TickerLabel key={label}>{label}</TickerLabel>
+      ))}
+    </TickerLabelContainer>
+  );
 }
 
 export default function TimeTickerDemo() {
@@ -56,11 +77,10 @@ export default function TimeTickerDemo() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <div>
-          <Card>
-            <Tickers />
-          </Card>
-        </div>
+        <Card>
+          <Tickers />
+          <TickerLabels />
+        </Card>
       </main>
     </>
   );
